b9c64b10c15f04a115bbb48416c403148d320840
Diyen Momjang <diyenmomjang@gmail.com>
2019-05-15 15:08:45 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import {SharedModule} from './modules/shared/shared.module';
     // DmfbCrudModule,
     // DmfbAuthModule,
     BrowserModule,
-    // MdlModule,
     FormsModule,
     ReactiveFormsModule,
     // UtilsModule,
